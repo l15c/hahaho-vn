561bb74c32cc6363d1294c918d05a652eabc2e1b
l15c <lehuuloc126@gmail.com>
2023-05-31 13:45:40 +0700

    path, hook activeLink

---
+++ routes/path.ts
@@ -0,0 +1,30 @@
+export const PATH = {
+  homepage: "/",
+  aboutUs: {
+    root: "/ve-chung-toi",
+    thuNgo: "/ve-chung-toi/thu-ngo",
+    hanhTrinh: "/ve-chung-toi/hanh-trinh-phat-trien",
+    lanhDao: "/ve-chung-toi/lanh-dao",
+  },
+  customer: {
+    root: "/khach-hang-&-doi-tac",
+    tieuBieu: "/khach-hang-&-doi-tac/khach-hang-tieu-bieu",
+    doiTac: "/khach-hang-&-doi-tac/doi-tac-cong-nghe",
+  },
+  products: {
+    root: "/san-pham",
+  },
+  quyTrinh: {
+    root: "/quy-trinh-dich-vu",
+  },
+  news: {
+    root: "/tin-tuc",
+    hahaho: "/tin-tuc/tin-hahaho",
+    suKien: "/tin-tuc/su-kien",
+    thongCao: "/tin-tuc/thong-cao-bao-chi",
+    newsletter: "/tin-tuc/newsletter",
+  },
+  contact: {
+    root: "/lien-he",
+  },
+};
