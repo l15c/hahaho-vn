6b55be29d5852ce72442f8cfe5a9c9a13feb46e5
l15c <lehuuloc126@gmail.com>
2023-06-11 13:33:20 +0700

    Remove file

--- sections/quy-trinh-dich-vu-va-hop-tac/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as Banner } from './banner';
-export { default as step } from '../../app/(main)/quy-trinh-dich-vu/step';
-export { default as QuyTrinh } from '../../app/(main)/quy-trinh-dich-vu/FlowItem';
